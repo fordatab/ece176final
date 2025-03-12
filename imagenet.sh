ce2212148ac14f93fbd42e3d72b761ba8641b388
Eric <ugcire132@gmail.com>
2025-03-12 15:04:17 -0700

    add dropout

--- imagenet.sh
+++ imagenet.sh
@@ -1,24 +1,24 @@
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar #training (138 gb)
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar  #validation
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_test_v10102019.tar #test
-wget https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz #devkit w/ labels
+# curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar  # training (138 GB)
+curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar  # validation
+# curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_test_v10102019.tar  # test
+curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz  # devkit w/ labels
 
 
 mkdir imagenet
 
 mkdir imagenet/train
 cd imagenet/train
-tar -xvf ILSVRC2012_img_train.tar
+# tar -xvf ILSVRC2012_img_train.tar
 cd ..
 
 mkdir imagenet/val
 cd imagenet/val
-tar -xvf ILSVRC2012_img_val.tar 
+# tar -xvf ILSVRC2012_img_val.tar 
 cd ..
 
 mkdir imagenet/test
 cd imagenet/val
-tar -xvf ILSVRC2012_img_test_v10102019.tar
+# tar -xvf ILSVRC2012_img_test_v10102019.tar
 cd ..
 
 mkdir imagenet/devkit
@@ -26,8 +26,8 @@ cd imagenet/devkit
 tar -xzvf image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
 cd ..
 
-rm ILSVRC2012_img_val.tar
-rm ILSVRC2012_devkit_t12.tar.gz
-rm ILSVRC2012_img_train.tar
-rm ILSVRC2012_img_test_v10102019.tar
+# rm ILSVRC2012_img_val.tar
+# rm ILSVRC2012_devkit_t12.tar.gz
+# rm ILSVRC2012_img_train.tar
+# rm ILSVRC2012_img_test_v10102019.tar
 
