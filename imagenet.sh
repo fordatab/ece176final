4f3faadc993957218bb5777c50feb3869595bb34
Eric <ugcire132@gmail.com>
2025-03-12 19:31:28 -0700

    fix script

--- imagenet.sh
+++ imagenet.sh
@@ -1,33 +1,27 @@
-# curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar  # training (138 GB)
-curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar  # validation
-# curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_test_v10102019.tar  # test
-curl -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz  # devkit w/ labels
+#!/bin/bash
 
+# Download ImageNet dataset
+curl -C - -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_train.tar  # training (138 GB)
+curl -C - -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_val.tar  # validation
+curl -C - -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_img_test_v10102019.tar  # test
+curl -C - -O https://image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz  # devkit w/ labels
 
-mkdir imagenet
+# Create directories
+mkdir -p imagenet/train imagenet/val imagenet/test imagenet/devkit
 
-mkdir imagenet/train
+# Extract datasets
 cd imagenet/train
-# tar -xvf ILSVRC2012_img_train.tar
-cd ..
+ tar -xvf ../../ILSVRC2012_img_train.tar
+cd ../val
+ tar -xvf ../../ILSVRC2012_img_val.tar
+cd ../test
+ tar -xvf ../../ILSVRC2012_img_test_v10102019.tar
+cd ../devkit
+ tar -xzvf ../../ILSVRC2012_devkit_t12.tar.gz
+cd ../..
 
-mkdir imagenet/val
-cd imagenet/val
-# tar -xvf ILSVRC2012_img_val.tar 
-cd ..
-
-mkdir imagenet/test
-cd imagenet/val
-# tar -xvf ILSVRC2012_img_test_v10102019.tar
-cd ..
-
-mkdir imagenet/devkit
-cd imagenet/devkit
-tar -xzvf image-net.org/data/ILSVRC/2012/ILSVRC2012_devkit_t12.tar.gz
-cd ..
-
-# rm ILSVRC2012_img_val.tar
-# rm ILSVRC2012_devkit_t12.tar.gz
+# Cleanup (uncomment to remove tar files after extraction)
 # rm ILSVRC2012_img_train.tar
+# rm ILSVRC2012_img_val.tar
 # rm ILSVRC2012_img_test_v10102019.tar
-
+# rm ILSVRC2012_devkit_t12.tar.gz
