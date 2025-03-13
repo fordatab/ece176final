8765f9239dfc7c3478d9f1924de1898d2df2df1c
s4chakraborty <165962039+s4chakraborty@users.noreply.github.com>
2025-03-13 16:42:01 -0700

    Update context_encoder.py

--- context_encoder.py
+++ context_encoder.py
@@ -156,7 +156,6 @@ if __name__ == "__main__":
     output = model(input_tensor)
     print("Output shape:", output.shape)
 
-
 ##################
 ##################
 ##################
@@ -218,14 +217,19 @@ class Decoder2(nn.Module):
         self.upconv3 = nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1)
         self.relu3 = nn.ReLU(inplace=True)
 
-        self.upconv4 = nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1)
+        self.upconv4 = nn.ConvTranspose2d(64, 64, kernel_size=4, stride=2, padding=1)
         self.relu4 = nn.ReLU(inplace=True)
+
+        self.upconv5 = nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1)
+        self.relu5 = nn.ReLU(inplace=True)
+
 
     def forward(self, x):
         x = self.relu1(self.upconv1(x))
         x = self.relu2(self.upconv2(x))
         x = self.relu3(self.upconv3(x))
         x = self.relu4(self.upconv4(x))
+        x = self.relu5(self.upconv5(x))
 
         return x
     
@@ -253,16 +257,29 @@ class Discriminator2(nn.Module):
     def __init__(self):
         super(Discriminator2, self).__init__()
         self.conv1 = nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1)
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)  
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)  
-        self.conv4 = nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1)  
-        self.fc = nn.Linear(8192, 1)  
+        self.relu1 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv2 = nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1)
+        self.relu2 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv3 = nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1)
+        self.relu3 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv4 = nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1)
+        self.relu4 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.conv5 = nn.Conv2d(512, 512, kernel_size=4, stride=2, padding=1)
+        self.relu5 = nn.LeakyReLU(0.2, inplace=True)
+
+        self.fc = nn.Linear(512 * 4 * 4, 1)
 
     def forward(self, x):
-        x = F.leaky_relu(self.conv1(x), 0.2)
-        x = F.leaky_relu(self.conv2(x), 0.2)
-        x = F.leaky_relu(self.conv3(x), 0.2)
-        x = F.leaky_relu(self.conv4(x), 0.2)
+        x = self.relu1(self.conv1(x))
+        x = self.relu2(self.conv2(x))
+        x = self.relu3(self.conv3(x))
+        x = self.relu4(self.conv4(x))
+        x = self.relu5(self.conv5(x))
+
         x = x.view(x.size(0), -1)
         x = self.fc(x)
         return x
