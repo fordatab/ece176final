098dcc590ab3a94ce9d61da6acd9312a9610a705
fordatab <90943649+fordatab@users.noreply.github.com>
2025-03-17 16:24:43 -0700

    Update discrim_model.py

--- discrim_model.py
+++ discrim_model.py
@@ -11,44 +11,44 @@ class ContextEncoder(nn.Module):
             nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(512, latent_dim, kernel_size=4, stride=1, padding=0, bias=False),
         )
-        
+
         # Decoder
         self.decoder = nn.Sequential(
             nn.ConvTranspose2d(latent_dim, 512, kernel_size=4, stride=1, padding=0, bias=False),
             nn.BatchNorm2d(512),
             nn.ReLU(inplace=True),
-            
+
             nn.ConvTranspose2d(512, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.ReLU(inplace=True),
-            
+
             nn.ConvTranspose2d(256, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.ReLU(inplace=True),
-            
+
             nn.ConvTranspose2d(128, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
-            
+
             nn.ConvTranspose2d(64, 3, kernel_size=4, stride=2, padding=1, bias=False),
-            nn.Tanh()
+            # nn.Tanh()
         )
-    
+
     def forward(self, x):
         latent = self.encoder(x)
         reconstructed = self.decoder(latent)
@@ -60,22 +60,22 @@ class Discriminator(nn.Module):
         self.model = nn.Sequential(
             nn.Conv2d(3, 64, kernel_size=4, stride=2, padding=1, bias=False),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(128),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(256),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1, bias=False),
             nn.BatchNorm2d(512),
             nn.LeakyReLU(0.2, inplace=True),
-            
+
             nn.Conv2d(512, 1, kernel_size=4, stride=1, padding=0, bias=False),
             nn.Sigmoid()
         )
-    
+
     def forward(self, x):
         return self.model(x)
